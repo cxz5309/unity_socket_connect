c8f7e0037ba2ea82dc9eaff1b529e05798d60d57
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2022-01-28 23:54:49 +0900

    [feat] asset 사용 socket 클라이언트 연결

--- client/Assets/Scripts/Manager/SocketManager.cs
+++ client/Assets/Scripts/Manager/SocketManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BestHTTP.SocketIO3;
+useing BestHttP.SocketIO3.Events;
 
 public class SocketManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        SocketIO3Connect();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SocketIO3Connect()
     {
-        
+        SocketOptions options = new SocketOptions();
+        options.AutoConnect = false;
+
+        socketManager = new SocketManager(new System.Uri(address), options);
+        socketManager.Open();
+
+        socketManager.Socket.On<ConnectResponse>(SocketIOEventTypes.Connect, OnConnected);
+        socketManager.Socket.On("PlayerConnected", PlayerConnected);
+    }
+
+    private void OnConnected(ConnectResponse res)
+    {
+        Debug.Log("Connected! Socket.IO");
+    }
+
+    private void PlayerConnected()
+    {
+        Debug.Log("Player Connected!!");
+    }
+
+    private void Destory()
+    {
+        if (this.socketManager != null)
+        {
+            this.socketManager.Close();
+            this.socketManager = null;
+        }
     }
 }
